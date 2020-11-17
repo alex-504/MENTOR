575e63449a1d25d27b93e224b2de9cf309bc685f
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2020-11-17 12:05:40 +0900

    Created show controller and show-view

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,2 +1,8 @@
 class MentorsController < ApplicationController
+  def show
+    #show 1 mentor page
+    @mentor = Mentor.find(params[:id])
+    @consultation = Consultation.new    
+    authorize @mentor
+  end
 end
