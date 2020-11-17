fb396801c1a2f2735d71d963757b3159d40835a3
aline utiyama <lineskutiyama@gmail.com>
2020-11-17 18:43:48 +0900

    Index page done

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,5 +2,6 @@ class Mentor < ApplicationRecord
   belongs_to :user
   has_many :consultations, dependent: :destroy
   has_many :mentor_tags, dependent: :destroy
+  has_many :tags, through: :mentor_tags
   validates :title, :availability, :price, :description, presence: true
 end
