78fc65a7fe91fd718bec20833bde14a530cd54dd
aline utiyama <lineskutiyama@gmail.com>
2020-11-18 13:49:37 +0900

    bug fixed

--- app/policies/mentor_policy.rb
+++ app/policies/mentor_policy.rb
@@ -1,22 +1,22 @@
 class MentorPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      scope.all
     end
   end
-  
+
   def index?
     true
   end
-  
+
   def new?
     true
   end
-  
+
   def create?
     true
   end
-    
+
   def show?
     true
   end
