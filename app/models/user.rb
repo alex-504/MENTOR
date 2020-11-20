fd908bf5706971dbdbe7744a85c0bc303ba80a83
aline utiyama <lineskutiyama@gmail.com>
2020-11-20 12:29:56 +0900

    style fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :mentors, dependent: :destroy
+  has_one :mentor, dependent: :destroy
   has_many :consultations, dependent: :destroy
   has_one_attached :photo
   validates :email, :password, presence: true
