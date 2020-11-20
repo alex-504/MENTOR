40e06d5e615cf6111d43c9a6d9b3a3d4dcd8cabb
shoneriki <ikirenohs@gmail.com>
2020-11-20 11:43:19 +0900

    seeds version with elementary cloudinary

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,7 +1,7 @@
 class Consultation < ApplicationRecord
   belongs_to :user
   belongs_to :mentor
-  has_many :consultation_tags
+  has_many :consultation_tags, dependent: :destroy
   has_many :tags, through: :consultation_tags
 
   validates :details, :start_time, :duration, presence: true
