18d6f160c2108e02066c6044858d267fac979bf3
shoneriki <ikirenohs@gmail.com>
2020-11-21 16:58:21 +0900

    reviews ajaxified, and minimal design, maybe come back to it later for style or extra ideas

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    review = Review.new
+  end
+
   def create
     @mentor = Mentor.find(params[:mentor_id])
     @review = Review.new(review_params)
+    @review.mentor = @mentor
+    authorize @review
+    if @review.save
+      redirect_to mentor_path(@mentor, anchor: "review-#{@review.id}")
+    else
+      redirect_back(fallback_location: root_path)
+    end
 
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :mentors do
     resources :consultations, only: [:new, :index, :create ]
-    resources :reviews, only: :create
+    resources :reviews, only: [:new, :create]
 
   end
 
