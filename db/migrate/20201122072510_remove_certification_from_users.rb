e4f6242ec2f0bc72ce4b6d39604652167de7e28d
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2020-11-23 11:38:35 +0900

    Commented out the remove migration to prevent the server to run the application.

--- db/migrate/20201122072510_remove_certification_from_users.rb
+++ db/migrate/20201122072510_remove_certification_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveCertificationFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :certification, :text
+    # remove_column :users, :certification, :text
   end
 end
