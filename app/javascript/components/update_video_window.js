080e2e6cdc583e367320bbbbd87adb51944b022c
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2020-11-25 13:19:14 +0900

    Started fixing video. Will come back and finish it

---
+++ app/javascript/components/update_video_window.js
@@ -0,0 +1,6 @@
+const updateVideoWindow = () => {
+  const title = document.querySelector('h6 .MuiTypography-h6');
+  console.log(title);
+  // title.innerHTML = 'Mentor Video';
+}
+export {updateVideoWindow}
